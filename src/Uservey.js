ec522bd3ca36c7fbb831859118b0783aca1edbb0
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-01-18 17:28:18 -0500

    first init

---
+++ src/Uservey.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Uservey extends Component {
+  render(){
+    return(
+      <div>
+        
+        I am from Uservey Component
+      
+      </div>
+    );
+  }
+}
+
+
+export default Uservey;

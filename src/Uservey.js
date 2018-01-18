a55bd48a70bb04a1ace79b4a3ce5f93b8b6a9b64
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-01-18 17:59:44 -0500

    firebase databass linked up

--- src/Uservey.js
+++ src/Uservey.js
@@ -1,4 +1,16 @@
 import React, { Component } from 'react';
+const firebase = require('firebase');
+const uuid = require('uuid');
+
+var config = {
+    apiKey: "AIzaSyAZCZGWrToSwFcvYkZOuTqYOIIqJg3hxJs",
+    authDomain: "uservey-3d3c7.firebaseapp.com",
+    databaseURL: "https://uservey-3d3c7.firebaseio.com",
+    projectId: "uservey-3d3c7",
+    storageBucket: "uservey-3d3c7.appspot.com",
+    messagingSenderId: "225411092565"
+  };
+  firebase.initializeApp(config);
 
 class Uservey extends Component {
   render(){

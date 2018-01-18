e8a67103b64c3559bb321d9818dc126412257002
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-01-18 18:21:46 -0500

    student input field gets console.loged wooooooooo

--- src/Uservey.js
+++ src/Uservey.js
@@ -11,18 +11,65 @@ var config = {
     messagingSenderId: "225411092565"
   };
   firebase.initializeApp(config);
-
+  
 class Uservey extends Component {
+  
+  nameSubmit(event){
+    let studentName = this.refs.name.value;
+    this.setState({studentName: studentName}, function(){
+      console.log(this.state)
+    })
+  }
+  
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      uid: uuid.v1(), 
+      studentName: '',
+      answers:{
+        answer1: '',
+        answer2: '',
+        answer3: ''
+      },
+      isSubmitted: false
+    };
+    
+    this.nameSubmit = this.nameSubmit.bind(this);
+    
+  }
+
+
   render(){
+    let studentName;
+    let questions;
+    
+    if(this.state.studentName === '' && this.state.isSubmitted === false){
+      studentName = <div>
+      <h1>Welcome to survay: </h1>
+      <form onSubmit={this.nameSubmit}>
+      
+      <input className="namy" type="text" placeholder="Insert your name" ref="name" />
+        
+      
+      </form>
+      
+      </div>
+    }
+    
     return(
       <div>
         
-        I am from Uservey Component
+      {studentName}
+      ----------------------------
+      {questions}
       
       </div>
     );
   }
 }
+
+
 
 
 export default Uservey;

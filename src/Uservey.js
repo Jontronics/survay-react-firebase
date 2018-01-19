d28ce1741de98821110ef9aef7c21518ca339537
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-01-18 19:03:24 -0500

    questions and submit button are in there

--- src/Uservey.js
+++ src/Uservey.js
@@ -18,7 +18,14 @@ class Uservey extends Component {
     let studentName = this.refs.name.value;
     this.setState({studentName: studentName}, function(){
       console.log(this.state)
-    })
+    });
+  }
+  answerSelected(){
+    
+  }
+  
+  questionSubmit(){
+    
   }
   
   constructor(props){
@@ -26,7 +33,7 @@ class Uservey extends Component {
     
     this.state = {
       uid: uuid.v1(), 
-      studentName: '',
+      studentName: 'test name JOn',
       answers:{
         answer1: '',
         answer2: '',
@@ -36,6 +43,8 @@ class Uservey extends Component {
     };
     
     this.nameSubmit = this.nameSubmit.bind(this);
+    this.answerSelected = this.answerSelected.bind(this);
+    this.questionSubmit = this.questionSubmit.bind(this);
     
   }
 
@@ -48,13 +57,37 @@ class Uservey extends Component {
       studentName = <div>
       <h1>Welcome to survay: </h1>
       <form onSubmit={this.nameSubmit}>
-      
       <input className="namy" type="text" placeholder="Insert your name" ref="name" />
-        
-      
       </form>
-      
       </div>
+      questions = '';
+    }else if (this.state.studentName !== '' && this.state.isSubmitted === false){
+      studentName = <h1> Hey, welcome to survay stuff, {this.state.studentName}</h1>;
+        questions = 
+        <div>
+          <h2>Here are some questions</h2>
+          <form onSubmit={this.questionSubmit}>
+            <div className="card">
+              <label>What do you think about this feature?</label> <br />
+              <input type="radio" name="answer1" value="Awesome" onChange={this.answerSelected}/> Awesome 
+              <input type="radio" name="answer2" value="It's ok" onChange={this.answerSelected}/> It's ok
+              <input type="radio" name="answer3" value="It's terrible" onChange={this.answerSelected}/> It's terrible 
+            </div>
+            <div className="card">
+              <label>Would you ever use this feature?</label> <br />
+              <input type="radio" name="answer1" value="Yes" onChange={this.answerSelected}/> Yes
+              <input type="radio" name="answer2" value="Sometimes" onChange={this.answerSelected}/> Sometimes
+              <input type="radio" name="answer3" value="Never, It's terrible" onChange={this.answerSelected}/> Never, It's terrible 
+            </div>
+            <div className="card">
+              <label>How many times a day do you use this app?</label> <br />
+              <input type="radio" name="answer1" value="Everyday" onChange={this.answerSelected}/> Everyday
+              <input type="radio" name="answer2" value="Sometimes" onChange={this.answerSelected}/> Every other day
+              <input type="radio" name="answer3" value="Never" onChange={this.answerSelected}/> Never 
+            </div>
+            <input className="feedback-button" type="submit" value="submit" />
+          </form>
+        </div>
     }
     
     return(
